c3323178fd179d57b0e5ece468f0b08b85641a06
umarbek003 <mejmanbekov72@gmail.com>
2023-09-11 00:51:14 +0600

    card

--- src/components/Ui/Card/Card.jsx
+++ src/components/Ui/Card/Card.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import './Card.scss'
+import React from "react";
+import "./Card.scss";
 
-const Card = () => {
+const Card = ({Variants, image, title, price}) => {
   return (
     <div className="card">
-        <button>Vegetable</button>
-        <div className="card-img">
-            <img src="/public/assets/jsonImg/Photo.png" alt="" />
-        </div>
+      <button className="card-variants">{Variants}</button>
+      <div className="card-img">
+        <img src={image} alt="" />
+      </div>
+      <div className="card-block-info">
         <div className="card-info">
-            <h2>Calabrese Broccoli</h2>
+          <h2>{title}</h2>
         </div>
         <div className="card-price">
-            <span>$13.00</span>
-            <span><img src="/public/assets/Star.png" alt="" /></span>
+          <p>${price}.00</p>
+          <span>
+            <img src="/public/assets/Star.png" alt="" />
+          </span>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-import './Shop.scss'
+import "./Shop.scss";
 import Card from "../../components/Ui/Card/Card";
 
 const Shop = () => {
+  const cardArr = {
+    Variants: "Veagetable",
+    Image: "/public/assets/jsonImg/Photo.png",
+    title: "Calabrese Broccoli",
+    price: 12,
+  };
+
   return (
     <>
-    <section className='shop' >
-      <div className="container">
-        <div className="shop-content">
-          <h1>Shop</h1>
+      <section className="shop">
+        <div className="container">
+          <div className="shop-content">
+            <h1>Shop</h1>
+          </div>
         </div>
+      </section>
+      <div className="container mt-20 d-flex  justify-between">
+        <Card
+          Variants={cardArr.Variants}
+          image={cardArr.Image}
+          title={cardArr.title}
+          price={cardArr.price}
+        />
       </div>
-
-    </section>
-          <Card/>
     </>
-  )
+  );
 };
 
 export default Shop;
