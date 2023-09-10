49b95592dba63119070eff2bf4858a19a4919bb1
umarbek003 <mejmanbekov72@gmail.com>
2023-09-10 07:42:10 +0600

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer">
+          <div className="footer-subscribe">
+            <div className="footer-title">
+              <h1>Subscribe to our Newsletter</h1>
+            </div>
+            <div className="footer-input">
+              <input type="text" />
+              <button>Subscripe</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
