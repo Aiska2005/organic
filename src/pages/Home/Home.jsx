38e9613940045399934aa223cd30f1b0657fb835
Aiska2005 <132543262+Aiska2005@users.noreply.github.com>
2023-09-10 21:01:41 +0600

    add end work

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <div className="homeImg"></div>
+      <div className="homeImg">
+        <div className="center_contant_home">
+          <h3>100% Natural Food</h3>
+          <h1>
+            Choose the best
+            <br />
+            healthier way
+            <br />
+            of life
+          </h1>
+          <button>
+            Explore Now
+            <BsFillArrowRightCircleFill />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
