474359cbbe47a4433adf52fb48c1868d2423bde0
Aiska2005 <132543262+Aiska2005@users.noreply.github.com>
2023-09-12 15:09:25 +0600

    col

--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -4,7 +4,13 @@ import "./Skeleton.scss";
 const Skeleton = () => {
   return (
     <div>
-      <div class="card"></div>
+      <div class="card">
+        <div class="card__image"></div>
+        <div class="card__content">
+          <h2></h2>
+          <p></p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Section_one/Section_one.jsx
+++ src/pages/Home/Section_one/Section_one.jsx
@@ -6,6 +6,8 @@ import "./Section_one.css";
 const Section_one = () => {
   return (
     <div className="contantSection container">
+      {/* <Skeleton /> */}
+
       <div className="photo">
         <div className="textCenter">
           <span className="natiral">Natural!!</span>
