5477514f2be986694025990f4b83935da3a49d29
Aiska2005 <132543262+Aiska2005@users.noreply.github.com>
2023-09-12 10:28:10 +0600

    animation

--- src/pages/Home/Section_three/Section_three.jsx
+++ src/pages/Home/Section_three/Section_three.jsx
@@ -6,7 +6,32 @@ const Section_three = () => {
   return (
     <div className="container section_three">
       <div className="bgImg"></div>
-      <div className="bgWhite"></div>
+      <div className="bgWhite">
+        <div className="width">
+          <span className="eco">Eco Friendly</span>
+          <h4 className="text_h4">
+            Econis is a Friendly <br />
+            Organic Store
+          </h4>
+          <div>
+            <span>Start with Our Company First</span>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptat
+              accusantium <br /> doloremque laudantium. Sed ut perspiciatis.
+            </p>
+            <span>Learn How to Grow Yourself</span>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptat
+              accusantium <br /> doloremque laudantium. Sed ut perspiciatis.
+            </p>
+            <span>Farming Strategies of Today</span>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptat
+              accusantium <br /> doloremque laudantium. Sed ut perspiciatis.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
