9d24fa30db7977a94109836a59e6e0383c52655b
Aiska2005 <132543262+Aiska2005@users.noreply.github.com>
2023-09-12 13:51:19 +0600

    add all

---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// css
+import "./Skeleton.scss";
+const Skeleton = () => {
+  return (
+    <div>
+      <div class="card"></div>
+    </div>
+  );
+};
+
+export default Skeleton;
--- src/pages/Home/Section_second/Section_second.jsx
+++ src/pages/Home/Section_second/Section_second.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../../components/Ui/Card/Card";
 // css
 import "./Section_second.css";
+import Skeleton from "../../../components/Skeleton/Skeleton";
 
 const Section_second = ({ item }) => {
+  const [isLoading, setLoading] = useState(false);
   return (
     <div className="container">
       <div style={{ textAlign: "center" }}>
@@ -11,11 +13,13 @@ const Section_second = ({ item }) => {
         <span className="textCategories">Categories</span>
         <h5>Our Products</h5>
       </div>
-      <div className="  blockAxios ">
-        {item.map((el) => (
-          <Card key={el.id} {...el} />
-        ))}
-      </div>
+      {
+        <div className="  blockAxios ">
+          {item.map((el) => (
+            <Card key={el.id} {...el} />
+          ))}
+        </div>
+      }
     </div>
   );
 };
